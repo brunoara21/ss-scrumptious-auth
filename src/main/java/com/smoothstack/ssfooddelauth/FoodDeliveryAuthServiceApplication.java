121126b02eb2f5c203224f9962a63e9b66a4ee15
Bruno Desktop <bruno.rebaza@smoothstack.com>
2021-08-16 15:01:15 -0400

    Initial Spring commit

---
+++ src/main/java/com/smoothstack/ssfooddelauth/FoodDeliveryAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smoothstack.ssfooddelauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoothstack/ssfooddelauth/FoodDeliveryAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.ssfooddelauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
